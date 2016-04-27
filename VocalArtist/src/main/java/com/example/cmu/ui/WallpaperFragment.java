ca53fbf7e46886a6c975e19e57b6f69710d03bbd
ken-tseee <junjianx@andrew.cmu.edu>
2016-04-27 16:23:13 -0400

    Update WallpaperFragment.java

--- VocalArtist/src/main/java/com/example/cmu/ui/WallpaperFragment.java
+++ VocalArtist/src/main/java/com/example/cmu/ui/WallpaperFragment.java
@@ -8,11 +8,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
-/**
- * Author: Junjian Xie
- * Email: junjianx@andrew.cmu.edu
- * Date: 15/11/11
- */
 public class WallpaperFragment extends Fragment implements View.OnClickListener {
     public WallpaperFragment() {
     }
